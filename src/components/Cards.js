e4070a35f5974696f7a7ed8800d943a57ab9cc00
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-03-24 23:54:42 +0530

    cards added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,38 +9,39 @@ function Cards() {
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
-            <CardItem 
-            src="img-9.jpg"
-            text="Explore the hidden waterfall deep inside the Amazon Jungle"
-            label="Adventure"
-            path="/services/activity"
+            <CardItem
+              src="kedarnath.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services/activity"
             ></CardItem>
-            <CardItem 
-            src="img-2.jpg"
-            text="Travel through the Islands of Bali in a Private Cruise"
-            label="Luxury"
-            path="/services/activity"
+            <CardItem
+              src="nainital.jpg"
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services/activity"
             ></CardItem>
           </ul>
           <ul className='cards__items'>
-            <CardItem 
-            src="img-4.jpg"
-            text="Experience Football on Top of the Himalayan Mountains"
-            label="Adventure"
-            path="/services/activity"
+            <CardItem
+              src="chardham.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="Adrenaline"
+              path="/services/activity"
             ></CardItem>
-            <CardItem 
-            src="img-3.jpg"
-            text="Set a Sail in the Atlantic ocean visiting Uncharted Waters"
-            label="Mystery"
-            path="/services/activity"
+            <CardItem
+              src="mussoorie.webp"
+              text="Experience Football on Top of the Himalayan Mountains"
+              label="Adventure"
+              path="/services/activity"
             ></CardItem>
-            <CardItem 
-            src="img-8.jpg"
-            text="Ride through the Sahara Desert on a guided camel tour"
-            label="Adrenaline"
-            path="/services/activity"
+            <CardItem
+              src="haridwar.webp"
+              text="Set a Sail in the Atlantic ocean visiting Uncharted Waters"
+              label="Mystery"
+              path="/services/activity"
             ></CardItem>
+
           </ul>
         </div>
       </div>
